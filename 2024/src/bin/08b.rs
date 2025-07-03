268858aa26b92de14547892d4413c03c768d4f87
Shawn M Moore <code@sartak.org>
2025-07-03 18:54:58 -0400

    2024 day 8

---
+++ 2024/src/bin/08b.rs
@@ -0,0 +1,53 @@
+use anyhow::Result;
+use itertools::Itertools;
+use std::collections::{HashMap, HashSet};
+
+fn main() -> Result<()> {
+    #[cfg(debug_assertions)]
+    let input = std::fs::read_to_string("input/08-example.txt")?;
+    #[cfg(not(debug_assertions))]
+    let input = std::fs::read_to_string("input/08.txt")?;
+
+    let mut antennae: HashMap<char, Vec<(i64, i64)>> = HashMap::new();
+
+    let mut ymax = 0;
+    let mut xmax = 0;
+
+    for (y, line) in input.lines().enumerate() {
+        if y as i64 > ymax {
+            ymax = y as i64;
+        }
+        for (x, c) in line.chars().enumerate() {
+            if x as i64 > xmax {
+                xmax = x as i64;
+            }
+            if c == '.' {
+                continue;
+            }
+
+            antennae.entry(c).or_default().push((x as i64, y as i64));
+        }
+    }
+
+    let mut antinodes = HashSet::new();
+    for antennae in antennae.values() {
+        'perm: for perm in antennae.iter().permutations(2) {
+            let (x0, y0) = perm[0];
+            let (x1, y1) = perm[1];
+            let dx = x0 - x1;
+            let dy = y0 - y1;
+            for n in 1.. {
+                let x = x0 - n * dx;
+                let y = y0 - n * dy;
+                if x < 0 || y < 0 || x > xmax || y > ymax {
+                    continue 'perm;
+                }
+                antinodes.insert((x, y));
+            }
+        }
+    }
+
+    println!("{}", antinodes.len());
+
+    Ok(())
+}
