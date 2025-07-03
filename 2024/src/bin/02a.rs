087fd8b9219a91a62358954b292386c56b57c34a
Shawn M Moore <code@sartak.org>
2025-07-03 15:30:20 -0400

    2024 day 2

---
+++ 2024/src/bin/02a.rs
@@ -0,0 +1,46 @@
+use anyhow::Result;
+use itertools::Itertools;
+
+fn main() -> Result<()> {
+    #[cfg(debug_assertions)]
+    let input = std::fs::read_to_string("input/02-example.txt")?;
+    #[cfg(not(debug_assertions))]
+    let input = std::fs::read_to_string("input/02.txt")?;
+
+    let mut safe_reports = 0;
+
+    for line in input.lines() {
+        let mut increasing = None;
+        let mut safe = true;
+
+        for (this, next) in line
+            .split_whitespace()
+            .map(|s| s.parse::<i32>().unwrap())
+            .tuple_windows()
+        {
+            if let Some(increasing) = increasing {
+                if increasing && next < this {
+                    safe = false;
+                } else if !increasing && this < next {
+                    safe = false;
+                }
+            } else {
+                increasing = Some(this < next);
+            }
+
+            if (next - this).abs() > 3 {
+                safe = false;
+            }
+            if next == this {
+                safe = false;
+            }
+        }
+
+        if safe {
+            safe_reports += 1;
+        }
+    }
+
+    println!("{safe_reports}");
+    Ok(())
+}
---
+++ 2024/src/bin/02b.rs
@@ -0,0 +1,56 @@
+use anyhow::Result;
+use itertools::Itertools;
+
+fn main() -> Result<()> {
+    #[cfg(debug_assertions)]
+    let input = std::fs::read_to_string("input/02-example.txt")?;
+    #[cfg(not(debug_assertions))]
+    let input = std::fs::read_to_string("input/02.txt")?;
+
+    let mut safe_reports = 0;
+
+    'line: for line in input.lines() {
+        let levels = line
+            .split_whitespace()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect_vec();
+
+        // -1 won't match any index, so it tries without skipping
+        for skip in -1..(levels.len() as i32) {
+            let mut increasing = None;
+            let mut safe = true;
+
+            for (this, next) in levels
+                .iter()
+                .enumerate()
+                .filter_map(|(i, level)| if i as i32 == skip { None } else { Some(level) })
+                .tuple_windows()
+            {
+                if let Some(increasing) = increasing {
+                    if increasing && next < this {
+                        safe = false;
+                    } else if !increasing && this < next {
+                        safe = false;
+                    }
+                } else {
+                    increasing = Some(this < next);
+                }
+
+                if (next - this).abs() > 3 {
+                    safe = false;
+                }
+                if next == this {
+                    safe = false;
+                }
+            }
+
+            if safe {
+                safe_reports += 1;
+                continue 'line;
+            }
+        }
+    }
+
+    println!("{safe_reports}");
+    Ok(())
+}
