e5226401cdd5211e34ed77b27f8a5fce84ac3e0b
Shawn M Moore <code@sartak.org>
2025-07-07 13:01:28 -0400

    2024 day 10

---
+++ 2024/src/bin/10a.rs
@@ -0,0 +1,69 @@
+use anyhow::Result;
+use itertools::Itertools;
+use std::collections::HashSet;
+
+fn score(trail: &[Vec<u32>], x: usize, y: usize) -> usize {
+    let mut seen = HashSet::new();
+    let mut queue = Vec::new();
+    queue.push((0, x as i32, y as i32));
+
+    while let Some((height, x, y)) = queue.pop() {
+        if height == 9 {
+            seen.insert((x, y));
+            continue;
+        }
+
+        for (dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+            let x = x + dx;
+            let y = y + dy;
+
+            if x < 0 || y < 0 {
+                continue;
+            }
+
+            let Some(row) = trail.get(y as usize) else {
+                continue;
+            };
+
+            let Some(&h) = row.get(x as usize) else {
+                continue;
+            };
+
+            if h == height + 1 {
+                queue.push((h, x, y));
+            }
+        }
+    }
+
+    seen.len()
+}
+
+fn main() -> Result<()> {
+    #[cfg(debug_assertions)]
+    let input = std::fs::read_to_string("input/10-example.txt")?;
+    #[cfg(not(debug_assertions))]
+    let input = std::fs::read_to_string("input/10.txt")?;
+
+    let trail = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap_or(99))
+                .collect_vec()
+        })
+        .collect_vec();
+
+    let mut answer = 0;
+    for (y, row) in trail.iter().enumerate() {
+        for (x, &height) in row.iter().enumerate() {
+            if height == 0 {
+                let s = score(&trail, x, y);
+                answer += s;
+            }
+        }
+    }
+
+    println!("{answer}");
+
+    Ok(())
+}
