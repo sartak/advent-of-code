a848ddaf4756fd94661d7d898d82e0b3ea82ac81
Shawn M Moore <code@sartak.org>
2025-07-08 21:56:22 -0400

    2024 day 25

---
+++ 2024/src/bin/25.rs
@@ -0,0 +1,63 @@
+use anyhow::Result;
+use itertools::Itertools;
+
+fn main() -> Result<()> {
+    #[cfg(debug_assertions)]
+    let input = std::fs::read_to_string("input/25-example.txt")?;
+    #[cfg(not(debug_assertions))]
+    let input = std::fs::read_to_string("input/25.txt")?;
+
+    let mut blocks = Vec::new();
+    let mut block = Vec::new();
+
+    for line in input.lines() {
+        if line.is_empty() {
+            blocks.push(block);
+            block = Vec::new();
+        } else {
+            block.push(line.chars().collect_vec());
+        }
+    }
+
+    let height = block.len() - 2;
+
+    blocks.push(block);
+
+    let mut keys = Vec::new();
+    let mut locks = Vec::new();
+
+    for block in blocks {
+        let parsed = (0..block[0].len())
+            .map(|x| {
+                (0..block.len())
+                    .map(|y| if block[y][x] == '#' { 1 } else { 0 })
+                    .sum::<usize>()
+                    - 1
+            })
+            .collect_vec();
+
+        if block[0] == vec!['#', '#', '#', '#', '#'] {
+            locks.push(parsed);
+        } else if *block.last().unwrap() == vec!['#', '#', '#', '#', '#'] {
+            keys.push(parsed);
+        } else {
+            panic!("Unhandled block {block:?}");
+        }
+    }
+
+    let mut answer = 0;
+    for key in &keys {
+        'lock: for lock in &locks {
+            for (&kc, &lc) in key.iter().zip(lock) {
+                if kc + lc > height {
+                    continue 'lock;
+                }
+            }
+            answer += 1;
+        }
+    }
+
+    println!("{answer}");
+
+    Ok(())
+}
